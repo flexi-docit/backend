3a848b77019fba7dc7202d7dbec64e18a41e419c
Inferno <inferno@192.168.1.12>
2023-02-12 21:05:48 +0530

    docker compose updated with env file

--- config/database.config.js
+++ config/database.config.js
@@ -4,17 +4,17 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB_NAME,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
   },
 };
--- index.js
+++ index.js
@@ -3,18 +3,12 @@ const dotenv = require("dotenv");
 const app = require("./config/server.js");
 const db = require("./models/index.js");
 const { mail } = require("./config/nodemailer.js");
-const emailQueue = require("./queues/email.queue.js");
 
 const logger = require("./utils/logger.js");
 
 dotenv.config();
 
 app.listen(process.env.PORT, async () => {
-  if (await emailQueue.isReady()) {
-    logger.info(`Redis Server up and running`);
-  } else {
-    throw new Error("No connection to Redis");
-  }
   await mail.verify();
   await db.sequelize.authenticate();
   await db.sequelize.sync();
